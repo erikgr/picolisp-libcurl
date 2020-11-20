b3d48467b50c656da2eb9719a6a199174d0e859b
neko <boop@the.snoot>
2020-11-19 22:59:41 -0500

    Add PUT, DELETE. Cleanup

--- compile_callbacks.sh
+++ compile_callbacks.sh
@@ -1,3 +1,5 @@
-gcc -c -Wall -Werror -fpic callbacks.c			\
-	&& gcc -shared -o callbacks.so callbacks.o	\
-	&& rm callbacks.o
+#!/bin/bash
+
+gcc -c -Wall -Werror -fpic pil_libcurl_callbacks.c				\
+	&& gcc -shared -o pil_libcurl_callbacks.so pil_libcurl_callbacks.o	\
+	&& rm pil_libcurl_callbacks.o
